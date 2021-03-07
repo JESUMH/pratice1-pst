cdc19df09332697a0411e145be2e18938e99f4c3
SOYUNPRO <support@soyunpro.com>
2021-03-07 19:06:53 +0100

    Add class method and define class controller

--- practice1_pst/src/exercise_1/MultiSphere.java
+++ practice1_pst/src/exercise_1/MultiSphere.java
@@ -4,6 +4,18 @@ public class MultiSphere {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Sphere sphere1 = new Sphere(2.0);
+		sphere1.setName("sphere1");
+		Sphere sphere2 = new Sphere(4.0);
+		sphere2.setName("sphere2");
+		System.out.println(sphere1.toString());
+		System.out.println(sphere2.toString());
+		
+		sphere1.setName("sphere1Modified");
+		sphere1.setDiameter(10.0);
+		
+		System.out.println(sphere1.toString());
+
 
 	}
 
--- practice1_pst/src/exercise_1/Sphere.java
+++ practice1_pst/src/exercise_1/Sphere.java
@@ -1,5 +1,42 @@
 package exercise_1;
 
 public class Sphere {
+	
+	private String name;
+	private Double diameter;
+	
+	public Sphere (Double diameter) {
+		this.diameter = diameter;
+	}
 
+	public Double getDiameter() {
+		return diameter;
+	}
+
+	public void setDiameter(Double diameter) {
+		this.diameter = diameter;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getVolumeOfSphere(Double diameter) {
+		return this.calculateVolumeSphere(diameter);
+	}
+	
+	private double calculateVolumeSphere(Double diameter) {
+		double volume = 0;
+		volume = 4*Math.PI*Math.pow(diameter/2,2);
+		return volume;
+	}
+
+	@Override
+	public String toString() {
+		return "Sphere name: " + name +"," +" diameter: " + diameter + "," + "volume: " + calculateVolumeSphere(diameter);
+	}
 }
